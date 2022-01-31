234700dcbfbb7bb05005a452841477852bbbe43c
nejdetkadir <nejdetkadir.550@gmail.com>
2022-01-31 23:44:53 +0300

    Set swagger ui as root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'swagger#index'
 
   # Swagger documentation
   scope :swagger do
-    get '/', to: 'swagger#index', as: 'swagger_root'
     get '/data', to: 'swagger#data', as: 'swagger_data'
   end
 
